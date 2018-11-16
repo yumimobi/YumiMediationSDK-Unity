2695a05de7aa3b36c770b90c75cb1327d485c278
ShunZhi Tang <tangshunzhi@zplay.com>
2018-11-16 16:45:01 +0800

    Fine tune splash only iOS

--- Assets/YumiMediationSDK/Platforms/iOS/YumiMediationSDKManager.cs
+++ Assets/YumiMediationSDK/Platforms/iOS/YumiMediationSDKManager.cs
@@ -4,160 +4,162 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class YumiMediationSDKManager : MonoBehaviour {
+public class YumiMediationSDKManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	// bannerEvent
-	public static event Action yumiMediationBannerViewDidLoadEvent;
-	public static event Action <string>yumiMediationSDKDidFailToReceiveAdEvent;
-	public static event Action yumiMediationBannerViewDidClickEvent;
+    }
 
-	// InterstitalEvent
-	public static event Action yumiMediationInterstitialDidReceiveAdEvent;
-	public static event Action <string>yumiMediationInterstitialDidFailToReceiveAdEvent;
-	public static event Action yumiMediationInterstitialWillDismissScreenEvent;
-	public static event Action yumiMediationInterstitialDidClickEvent;
+    // Update is called once per frame
+    void Update()
+    {
 
-	// videoEvent
-	public static event Action yumiMediationVideoDidOpenEvent;
-	public static event Action yumiMediationVideoDidStartPlayingEvent;
-	public static event Action yumiMediationVideoDidCloseEvent;
-	public static event Action yumiMediationVideoDidRewardEvent;
+    }
 
-	//splash
-	public static event Action yumiAdsSplashDidLoadEvent;
-	public static event Action <string>yumiAdsSplashDidFailToLoadEvent;
-	public static event Action yumiAdsSplashDidClickEvent;
-	public static event Action yumiAdsSplashDidClosedEvent;
-	public static event Func<Image> yumiAdsSplashDefaultImageEvent;
+    //// bannerEvent
+    //public static event Action yumiMediationBannerViewDidLoadEvent;
+    //public static event Action<string> yumiMediationSDKDidFailToReceiveAdEvent;
+    //public static event Action yumiMediationBannerViewDidClickEvent;
 
-	void Awake()
-	{
-		// Set the GameObject name to the class name for easy access from Obj-C
-		gameObject.name = this.GetType().ToString();
-	}
+    //// InterstitalEvent
+    //public static event Action yumiMediationInterstitialDidReceiveAdEvent;
+    //public static event Action<string> yumiMediationInterstitialDidFailToReceiveAdEvent;
+    //public static event Action yumiMediationInterstitialWillDismissScreenEvent;
+    //public static event Action yumiMediationInterstitialDidClickEvent;
 
-	//banner delegate
-	public void yumiMediationBannerViewDidLoad()
-	{
+    //// videoEvent
+    //public static event Action yumiMediationVideoDidOpenEvent;
+    //public static event Action yumiMediationVideoDidStartPlayingEvent;
+    //public static event Action yumiMediationVideoDidCloseEvent;
+    //public static event Action yumiMediationVideoDidRewardEvent;
+
+    ////splash
+    //public static event Action yumiAdsSplashDidLoadEvent;
+    //public static event Action<string> yumiAdsSplashDidFailToLoadEvent;
+    //public static event Action yumiAdsSplashDidClickEvent;
+    //public static event Action yumiAdsSplashDidClosedEvent;
+    //public static event Func<Image> yumiAdsSplashDefaultImageEvent;
+
+    void Awake()
+    {
+        // Set the GameObject name to the class name for easy access from Obj-C
+        gameObject.name = this.GetType().ToString();
+    }
+
+    //banner delegate
+    public void yumiMediationBannerViewDidLoad()
+    {
 
         YumiSDKEventHandler.OnYumiAdBannerDidLoad();
-	}
+    }
 
-	public void yumiMediationSDKDidFailToReceiveAd(string error)
-	{
+    public void yumiMediationSDKDidFailToReceiveAd(string error)
+    {
 
         YumiSDKEventHandler.OnYumiAdBannerDidFailToLoad(error);
 
     }
 
-	public void yumiMediationBannerViewDidClick()
-	{
-       
+    public void yumiMediationBannerViewDidClick()
+    {
+
         YumiSDKEventHandler.OnYumiAdBannerDidClick();
 
     }
 
 
-	// interstital delegate
-	public void yumiMediationInterstitialDidReceiveAd()
-	{
-       
+    // interstital delegate
+    public void yumiMediationInterstitialDidReceiveAd()
+    {
+
         YumiSDKEventHandler.OnYumiAdInterstitialDidLoad();
 
     }
 
-	public void yumiMediationInterstitialDidFailToReceiveAd(string error)
-	{
-       
+    public void yumiMediationInterstitialDidFailToReceiveAd(string error)
+    {
+
         YumiSDKEventHandler.OnYumiAdInterstitialDidFailToLoad(error);
 
     }
 
-	public void yumiMediationInterstitialWillDismissScreen()
-	{
-       
+    public void yumiMediationInterstitialWillDismissScreen()
+    {
+
         YumiSDKEventHandler.OnYumiAdInterstitialDidClose();
 
     }
 
-	public void yumiMediationInterstitialDidClick(){
-       
+    public void yumiMediationInterstitialDidClick()
+    {
+
         YumiSDKEventHandler.OnYumiAdInterstitialDidClick();
 
     }
 
-	// video delegate
-	public void yumiMediationVideoDidOpen()
-	{
-       
+    // video delegate
+    public void yumiMediationVideoDidOpen()
+    {
+
         YumiSDKEventHandler.OnYumiAdVideoDidOpen();
 
     }
 
-	public void yumiMediationVideoDidStartPlaying()
-	{
-       
+    public void yumiMediationVideoDidStartPlaying()
+    {
+
         YumiSDKEventHandler.OnYumiAdVideoDidStartPlaying();
 
     }
 
-	public void yumiMediationVideoDidClose()
-	{
-       
+    public void yumiMediationVideoDidClose()
+    {
+
         YumiSDKEventHandler.OnYumiAdVideoDidClose();
 
     }
 
-	public void yumiMediationVideoDidReward()
-	{
-       
+    public void yumiMediationVideoDidReward()
+    {
+
         YumiSDKEventHandler.OnYumiAdVideoDidReward();
 
     }
 
-	// splash
-	public void yumiAdsSplashDidLoad()
-	{
-		if (yumiAdsSplashDidLoadEvent != null) 
-		{
-			yumiAdsSplashDidLoadEvent ();
-		}
-	}
-	public void yumiAdsSplashDidFailToLoad(string error)
-	{
-		if (yumiAdsSplashDidFailToLoadEvent != null) 
-		{
-			yumiAdsSplashDidFailToLoadEvent (error);
-		}
-	}
-	public void yumiAdsSplashDidClick()
-	{
-		if (yumiAdsSplashDidClickEvent != null) {
-			yumiAdsSplashDidClickEvent ();
-		}
-	}
-	public void yumiAdsSplashDidClosed()
-	{
-		if (yumiAdsSplashDidClosedEvent != null) {
-			yumiAdsSplashDidClosedEvent ();
-		}
-	}
-	public Image yumiAdsSplashDefaultImage()
-	{
-		if (yumiAdsSplashDefaultImageEvent != null) {
-			return yumiAdsSplashDefaultImageEvent ();
-		}
-		return null;
-	}
+    // splash
+#if UNITY_IOS
+    public void yumiAdsSplashDidLoad()
+    {
+
+        YumiSDKEventHandler.OnYumiAdSplashDidLoad();
+
+    }
+    public void yumiAdsSplashDidFailToLoad(string error)
+    {
+
+        YumiSDKEventHandler.OnYumiAdSplashDidFailToLoad(error);
+
+    }
+    public void yumiAdsSplashDidClick()
+    {
+
+        YumiSDKEventHandler.OnYumiAdSplashDidClick();
+
+    }
+    public void yumiAdsSplashDidClosed()
+    {
+
+        YumiSDKEventHandler.OnYumiAdSplashDidClose();
+
+    }
+    public Image yumiAdsSplashDefaultImage()
+    {
+
+        return null;
+    }
+#endif
 
 }
