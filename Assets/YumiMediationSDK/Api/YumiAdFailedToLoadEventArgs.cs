eb0c4ccf3819a414c10b0b85c7239c8871b4c3ed
ShunZhi Tang <tangshunzhi@zplay.com>
2018-11-19 19:26:05 +0800

    Add bannerView api

---
+++ Assets/YumiMediationSDK/Api/YumiAdFailedToLoadEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace YumiMediationSDK.Api
+{
+
+    // Event that occurs when an ad fails to load.
+    public class YumiAdFailedToLoadEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+}
