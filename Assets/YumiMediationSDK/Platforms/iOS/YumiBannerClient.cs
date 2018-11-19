96b90a4b2bcc5d56a043237600f92019d3a80f2e
ShunZhi Tang <tangshunzhi@zplay.com>
2018-11-19 19:29:06 +0800

    iOS DllImport

---
+++ Assets/YumiMediationSDK/Platforms/iOS/YumiBannerClient.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using YumiMediationSDK.Common;
+using YumiMediationSDK.Api;
+
+
+namespace YumiMediationSDK.iOS
+{
+    public class YumiBannerClient : IYumiBannerClient
+    {
+
+        private IntPtr bannerViewPtr;
+
+        private IntPtr bannerClientPtr;
+
+        #region Banner callback types
+
+        internal delegate void YumiBannerDidReceiveAdCallback(IntPtr bannerClient);
+
+        internal delegate void YumiBannerDidFailToReceiveAdWithErrorCallback(
+                IntPtr bannerClient, string error);
+
+        internal delegate void YumiBannerDidClickCallback(IntPtr bannerClient);
+
+        #endregion
+
+        public event EventHandler<EventArgs> OnAdLoaded;
+        // Ad event fired when the banner ad has failed to load.
+        public event EventHandler<YumiAdFailedToLoadEventArgs> OnAdFailedToLoad;
+        // Ad event fired when the banner ad is click.
+        public event EventHandler<EventArgs> OnAdClick;
+
+
+        // This property should be used when setting the bannerViewPtr.
+        private IntPtr BannerViewPtr
+        {
+            get
+            {
+                return this.bannerViewPtr;
+            }
+
+            set
+            {
+                YumiExterns.YumiRelease(this.bannerViewPtr); // clear cache ,if existed
+                this.bannerViewPtr = value;
+            }
+        }
+
+
+        #region IYumiBannerClient implement 
+        public void CreateBannerView(string placementId, string channelId, string versionId, YumiAdPosition adPosition)
+        {
+            // A new GCHandle that protects the object from garbage collection. This GCHandle must be released with Free() when it is no longer needed.
+            this.bannerClientPtr = (IntPtr)GCHandle.Alloc(this);
+
+            this.BannerViewPtr = YumiExterns.InitYumiBannerAd(this.bannerClientPtr, placementId, channelId, versionId, (int)adPosition);
+
+            YumiExterns.SetBannerCallbacks(
+                this.BannerViewPtr,
+                BannerDidReceiveAdCallback,
+                BannerDidFailToReceiveAdWithErrorCallback,
+                BannerDidClickCallback);
+
+        }
+
+        // Requests a new ad for the banner view.
+        public void LoadAd(bool isSmart)
+        {
+            Logger.LogError("load ad");
+            YumiExterns.RequestBannerAd(this.BannerViewPtr,isSmart);
+        }
+
+        // Shows the banner view on the screen.
+        public void ShowBannerView(){
+            YumiExterns.ShowBannerView(this.BannerViewPtr);
+        }
+
+        // Hides the banner view from the screen.
+        public void HideBannerView(){
+            YumiExterns.HideBannerView(this.BannerViewPtr);
+        }
+
+        // Destroys a banner view.
+        public void DestroyBannerView(){
+
+            YumiExterns.DestroyBannerView(this.BannerViewPtr);
+            this.BannerViewPtr = IntPtr.Zero;
+        }
+        #endregion
+        #region Banner callback methods
+
+
+        [MonoPInvokeCallback(typeof(YumiBannerDidReceiveAdCallback))]
+        private static void BannerDidReceiveAdCallback(IntPtr bannerClient)
+        {
+            YumiBannerClient client = IntPtrToBannerClient(bannerClient);
+            if (client.OnAdLoaded != null)
+            {
+                client.OnAdLoaded(client, EventArgs.Empty);
+            }
+        }
+        [MonoPInvokeCallback(typeof(YumiBannerDidFailToReceiveAdWithErrorCallback))]
+        private static void BannerDidFailToReceiveAdWithErrorCallback(IntPtr bannerClient, string error)
+        {
+            YumiBannerClient client = IntPtrToBannerClient(bannerClient);
+            if (client.OnAdFailedToLoad != null)`
+            {
+                YumiAdFailedToLoadEventArgs args = new YumiAdFailedToLoadEventArgs()
+                {
+                    Message = error
+                };
+                client.OnAdFailedToLoad(client, args);
+            }
+        }
+        [MonoPInvokeCallback(typeof(YumiBannerDidClickCallback))]
+        private static void BannerDidClickCallback(IntPtr bannerClient)
+        {
+            YumiBannerClient client = IntPtrToBannerClient(bannerClient);
+            if (client.OnAdClick != null)
+            {
+                client.OnAdClick(client, EventArgs.Empty);
+            }
+        }
+
+
+        private static YumiBannerClient IntPtrToBannerClient(IntPtr bannerClient)
+        {
+            GCHandle handle = (GCHandle)bannerClient;
+            return handle.Target as YumiBannerClient;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Assets/YumiMediationSDK/Platforms/iOS/YumiExterns.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace YumiMediationSDK.iOS
+{
+    public class YumiExterns
+    {
+        #region
+
+        public enum YumiMediationAdViewBannerSize
+        {
+            /// iPhone and iPod Touch ad size. Typically 320x50.
+            kYumiMediationAdViewBanner320x50 = 1 << 0,
+            // Leaderboard size for the iPad. Typically 728x90.
+            kYumiMediationAdViewBanner728x90 = 1 << 1,
+            // Represents the fixed banner ad size - 300pt by 250pt.
+            kYumiMediationAdViewBanner300x250 = 1 << 2
+        }
+
+        [DllImport("__Internal")]
+        internal static extern void YumiRelease(IntPtr obj);
+
+        // banner
+        [DllImport("__Internal")]
+        internal static extern IntPtr InitYumiBannerAd(IntPtr bannerView,string placementID, string channelID, string versionID, int position);
+        [DllImport("__Internal")]
+        internal static extern void RequestBannerAd(IntPtr bannerView, bool isSmartBanner);
+        [DllImport("__Internal")]
+        internal static extern void ShowBannerView(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void HideBannerView(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void DestroyBannerView(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void SetBannerAdSize(IntPtr bannerView,YumiMediationAdViewBannerSize bannerSize);
+        [DllImport("__Internal")]
+        internal static extern void SetBannerCallbacks(
+           IntPtr bannerView,
+           YumiBannerClient.YumiBannerDidReceiveAdCallback adReceivedCallback,
+           YumiBannerClient.YumiBannerDidFailToReceiveAdWithErrorCallback adFailedCallback,
+           YumiBannerClient.YumiBannerDidClickCallback adClickedCallback);
+
+        #endregion
+      
+    }
+}
