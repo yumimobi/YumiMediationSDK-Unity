1e4464f61e7f3b1091f22eb9301f72320d7316cf
ShunZhi Tang <tangshunzhi@zplay.com>
2018-11-21 12:34:04 +0800

    Implement dummy client

--- Assets/YumiMediationSDK/Common/YumiDummyClient.cs
+++ Assets/YumiMediationSDK/Common/YumiDummyClient.cs
@@ -3,7 +3,7 @@ using YumiMediationSDK.Api;
 
 namespace YumiMediationSDK.Common
 {
-    public class YumiDummyClient : IYumiBannerClient
+    public class YumiDummyClient : IYumiBannerClient,IYumiInterstitialClient,IYumiRewardVideoClient
     {
         public YumiDummyClient()
         {
@@ -16,32 +16,91 @@ namespace YumiMediationSDK.Common
         public event EventHandler<YumiAdFailedToLoadEventArgs> OnAdFailedToLoad;
         // Ad event fired when the banner ad is click.
         public event EventHandler<EventArgs> OnAdClick;
+
+        // Ad event fired when the reward based video ad is opened.
+        public event EventHandler<EventArgs> OnAdOpening;
+        // Ad event fired when the reward based video ad has started playing.
+        public event EventHandler<EventArgs> OnAdStartPlaying;
+        // Ad event fired when the reward based video ad has rewarded the user.
+        public event EventHandler<EventArgs> OnAdRewarded;
+        // Ad event fired when the reward based video ad is closed.
+        public event EventHandler<EventArgs> OnAdClosed;
        
+        // Ad event fired when the interstitial ad is clicked.
+        public event EventHandler<EventArgs> OnAdClicked;
+
 #pragma warning restore 67
         // banner method
         // Creates a banner view and adds it to the view hierarchy.
         public void CreateBannerView(string placementId, string channelId, string versionId, YumiAdPosition adPosition){
-            Logger.LogError("create banner");
+            Logger.LogError("Dummy : create banner");
         }
 
         // Requests a new ad for the banner view.
         public void LoadAd(bool isSmart){
-            Logger.LogError("load ad");
+            Logger.LogError("Dummy :load ad");
         }
 
         // Shows the banner view on the screen.
         public void ShowBannerView(){
-            Logger.LogError("ShowBannerView");
+            Logger.LogError("Dummy: ShowBannerView");
         }
 
         // Hides the banner view from the screen.
         public void HideBannerView(){
-            Logger.LogError("HideBannerView");
+            Logger.LogError("Dummy: HideBannerView");
         }
 
         // Destroys a banner view.
         public void DestroyBannerView(){
-            Logger.LogError("DestroyBannerView");
+            Logger.LogError("Dummy: DestroyBannerView");
+        }
+
+        // Creates an InterstitialAd.
+        public void CreateInterstitialAd(string placementId, string channelId, string versionId){
+            Logger.LogError("Dummy: CreateInterstitialAd");
+        }
+
+        // Determines whether the interstitial has loaded.
+        public bool IsInterstitialReady(){
+            Logger.LogError("Dummy: IsInterstitialReady");
+            return false;
+        }
+
+        // Shows the InterstitialAd.
+        public void ShowInterstitial(){
+            Logger.LogError("Dummy: ShowInterstitial");
+        }
+
+        // Destroys an InterstitialAd.
+        public void DestroyInterstitial(){
+            Logger.LogError("Dummy: DestroyInterstitial");
+        }
+
+        //vdieo 
+        // Creates an RewardVideo.
+        public void CreateRewardVideoAd(){
+            Logger.LogError("Dummy: CreateRewardVideoAd");
+        }
+        // load RewardVideo
+        public void LoadRewardVideoAd(string placementId, string channelId, string versionId){
+            Logger.LogError("Dummy: LoadRewardVideoAd");
+        }
+
+        // Determines whether the interstitial has loaded.
+        public  bool IsRewardVideoReady(){
+            Logger.LogError("Dummy: IsRewardVideoReady");
+            return false;
+        }
+
+        // Shows the RewardVideo.
+        public  void PlayRewardVideo(){
+            Logger.LogError("Dummy: PlayRewardVideo");
+        }
+
+        // Destroys an RewardVideo.
+        public void DestroyRewardVideo(){
+            Logger.LogError("Dummy: DestroyRewardVideo");
         }
 
     }
