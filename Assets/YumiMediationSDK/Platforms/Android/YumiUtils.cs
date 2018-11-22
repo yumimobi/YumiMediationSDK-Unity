2076843985ac66bfa05338cce9ce7244ad7c7e7d
ShunZhi Tang <tangshunzhi@zplay.com>
2018-11-22 10:24:10 +0800

    Add YumiUtils

---
+++ Assets/YumiMediationSDK/Platforms/Android/YumiUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace YumiMediationSDK.Android
+{
+    public class YumiUtils
+    {
+
+        #region Unity class names
+
+        public const string UnityActivityClassName = "com.unity3d.player.UnityPlayer";
+
+        #endregion
+
+        #region Yumi  Ads Unity Plugin class names
+
+        public const string BannerViewClassName = "com.zplay.unity.adsyumi.YumiUBanner";
+
+        //public const string InterstitialClassName = "com.google.unity.ads.Interstitial";
+
+        //public const string RewardBasedVideoClassName = "com.google.unity.ads.RewardBasedVideo";
+
+        //public const string NativeAdLoaderClassName = "com.google.unity.ads.NativeAdLoader";
+
+        public const string UnityBannerAdListenerClassName = "com.zplay.unity.adsyumi.YumiUBannerListener";
+
+        public const string UnityRewardBasedVideoAdListenerClassName =
+            "com.google.unity.ads.UnityRewardBasedVideoAdListener";
+
+        public const string UnityAdLoaderListenerClassName =
+            "com.google.unity.ads.UnityAdLoaderListener";
+
+        public const string PluginUtilsClassName = "com.google.unity.ads.PluginUtils";
+       
+        #endregion
+    }
+}
