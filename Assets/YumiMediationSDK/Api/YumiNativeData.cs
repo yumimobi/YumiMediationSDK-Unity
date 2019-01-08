3f1e53887e97e084b4561bac92150b794d21f6a6
ShunZhi Tang <tangshunzhi@zplay.com>
2019-01-08 11:12:16 +0800

    Add YumiNativeData

---
+++ Assets/YumiMediationSDK/Api/YumiNativeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace YumiMediationSDK.Api
+{
+    public struct YumiNativeData
+    {
+        public string title;
+        public string desc;
+        public string iconURL;
+        public string coverImageURL;
+        public string appPrice;
+        public string appRating;
+        public string callToAction;
+        public string other;
+
+    }
+}
