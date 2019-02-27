60f3efab1365ced39de3c94f7db07063db375071
ShunZhi Tang <tangshunzhi@zplay.com>
2019-02-27 14:20:15 +0800

    Add Yumi native scene

---
+++ Assets/Samples/Scene/YumiNativeAd/YumiNativeScene.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using YumiMediationSDK.Api;
+using YumiMediationSDK.Common;
+
+[RequireComponent(typeof(CanvasRenderer))]
+[RequireComponent(typeof(RectTransform))]
+public class YumiNativeScene : MonoBehaviour
+{
+    //private NativeAd nativeAd;
+
+    // UI elements in scene
+    [Header("Text:")]
+    public Text title;
+    public Text body;
+    [Header("Images:")]
+    public GameObject mediaView;
+    public GameObject iconImage;
+    [Header("Buttons:")]
+    // This doesn't be a button - it can also be an image
+    public Button callToActionButton;
+    //[Header("Ad Choices:")]
+    //public AdChoices adChoices;
+
+    void Awake()
+    {
+        Logger.Log("Native ad ready to load.");
+        this.title.text = "test title";
+        this.body.text = "test body";
+        callToActionButton.GetComponentInChildren<Text>().text = "安装";
+    }
+
+    void OnDestroy()
+    {
+        // Dispose of native ad when the scene is destroyed
+        //if (this.nativeAd) {
+        //    this.nativeAd.Dispose();
+        //}
+        Logger.Log("NativeAdTest was destroyed!");
+    }
+
+    // Load Ad button
+    public void LoadAd()
+    {
+        Logger.Log("load ad ");
+    }
+
+    private void Log(string s)
+    {
+
+        Logger.Log(s);
+    }
+
+    // Next button
+    public void NextScene()
+    {
+        SceneManager.LoadScene("YumiScene");
+    }
+}
