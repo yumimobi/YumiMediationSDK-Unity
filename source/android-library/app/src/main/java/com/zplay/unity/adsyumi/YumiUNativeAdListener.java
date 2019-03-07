adb2f2bd8c68368e535cad55ec42e8636a45c156
zmer007 <developer.lgd@gmail.com>
2019-03-07 15:09:05 +0800

    update android sdk version and plugin jar

--- source/android-library/app/src/main/java/com/zplay/unity/adsyumi/YumiUNativeAdListener.java
+++ source/android-library/app/src/main/java/com/zplay/unity/adsyumi/YumiUNativeAdListener.java
@@ -2,7 +2,7 @@ package com.zplay.unity.adsyumi;
 
 public interface YumiUNativeAdListener {
 
-    void onLayerPrepared(int count);
+    void onLayerPrepared(String uniqueIds);
 
     void onLayerFailed(String errorMsg);
 
