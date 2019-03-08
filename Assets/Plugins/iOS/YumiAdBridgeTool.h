102ac30af501f5f7d69d98a6a31707ee55d5a8c6
ShunZhi Tang <tangshunzhi@zplay.com>
2019-03-08 17:31:58 +0800

    Add YumiNativeBridgeView

---
+++ Assets/Plugins/iOS/YumiAdBridgeTool.h
@@ -0,0 +1,18 @@
+//
+//  YumiAdBridgeTool.h
+//  Unity-iPhone
+//
+//  Created by Michael Tang on 2019/3/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiAdBridgeTool : NSObject
+
++(UIColor *)colorWithARGB:(NSUInteger)ARGBValue;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Assets/Plugins/iOS/YumiNativeBridgeView.m
@@ -0,0 +1,104 @@
+//
+//  YumiNativeBridgeView.m
+//  Unity-iPhone
+//
+//  Created by Michael Tang on 2019/3/8.
+//
+
+#import "YumiNativeBridgeView.h"
+#import "YumiAdBridgeTool.h"
+
+@implementation YumiNativeBridgeView
+
+- (void)setTitleTextColor:(uint)textColor textBgColor:(uint)textBgColor fontSize:(int)fontSize{
+    self.titleLab.textColor = [YumiAdBridgeTool colorWithARGB:textColor];
+    self.titleLab.backgroundColor = [YumiAdBridgeTool colorWithARGB:textBgColor];
+    self.titleLab.font = [UIFont systemFontOfSize:fontSize];
+}
+- (void)setDescTextColor:(uint)textColor textBgColor:(uint)textBgColor fontSize:(int)fontSize{
+    self.descLab.textColor = [YumiAdBridgeTool colorWithARGB:textColor];
+    self.descLab.backgroundColor = [YumiAdBridgeTool colorWithARGB:textBgColor];
+    self.descLab.font = [UIFont systemFontOfSize:fontSize];
+}
+- (void)setCallToActionTextColor:(uint)textColor textBgColor:(uint)textBgColor fontSize:(int)fontSize{
+    self.actLab.textColor = [YumiAdBridgeTool colorWithARGB:textColor];
+    self.actLab.backgroundColor = [YumiAdBridgeTool colorWithARGB:textBgColor];
+    self.actLab.font = [UIFont systemFontOfSize:fontSize];
+}
+- (void)setIconScaleType:(int)scaleType{
+    switch (scaleType) {
+        case 0:
+            self.iconView.contentMode = UIViewContentModeScaleToFill;
+            break;
+        case 1:
+            self.iconView.contentMode = UIViewContentModeScaleAspectFit;
+            break;
+        case 2:
+            self.iconView.contentMode = UIViewContentModeScaleAspectFill;
+            break;
+            
+        default:
+            break;
+    }
+    
+}
+- (void)setCoverImageScaleType:(int)scaleType{
+    switch (scaleType) {
+        case 0:
+            self.mediaView.contentMode = UIViewContentModeScaleToFill;
+            break;
+        case 1:
+            self.mediaView.contentMode = UIViewContentModeScaleAspectFit;
+            break;
+        case 2:
+            self.mediaView.contentMode = UIViewContentModeScaleAspectFill;
+            break;
+            
+        default:
+            break;
+    }
+}
+
+#pragma mark: getter
+
+-(UIImageView *)iconView{
+    if (!_iconView) {
+        _iconView = [[UIImageView alloc] init];
+        [self addSubview:_iconView];
+    }
+    return _iconView;
+}
+- (UIImageView *)mediaView{
+    if (!_mediaView) {
+        _mediaView = [[UIImageView alloc] init];
+        [self addSubview:_mediaView];
+    }
+    return _mediaView;
+}
+- (UILabel *)titleLab{
+    if (!_titleLab) {
+        _titleLab = [[UILabel alloc] init];
+        _titleLab.numberOfLines = 0;
+        [self addSubview:_titleLab];
+    }
+    return _titleLab;
+}
+
+- (UILabel *)descLab{
+    if (!_descLab) {
+        _descLab = [[UILabel alloc] init];
+        _descLab.numberOfLines = 0;
+        [self addSubview:_descLab];
+    }
+    return _descLab;
+}
+- (UILabel *)actLab{
+    if (!_actLab) {
+        _actLab = [[UILabel alloc] init];
+        _actLab.numberOfLines = 0;
+        [self addSubview:_actLab];
+    }
+    return _actLab;
+}
+
+@end
