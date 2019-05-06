08022b5148221614c1203d482d4b347f9b7d2eaf
ShunZhi Tang <tangshunzhi@zplay.com>
2019-05-06 11:03:54 +0800

    Add new event args

---
+++ Assets/YumiMediationSDK/Api/YumiAdFailedToShowEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace YumiMediationSDK.Api
+{
+    // Event that occurs when an ad fails to show.
+    public class YumiAdFailedToShowEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+}
