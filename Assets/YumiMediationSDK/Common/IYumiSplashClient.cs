9e70488af4bf970c235f1580d9e68bb2adba9b53
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-18 16:11:08 +0800

    Add IYumiSplashClient

---
+++ Assets/YumiMediationSDK/Common/IYumiSplashClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using YumiMediationSDK.Api;
+
+namespace YumiMediationSDK.Common
+{
+    public interface IYumiSplashClient
+    {
+        // Ad event fired when the splash ad success to show 
+        event EventHandler<EventArgs> OnAdSuccessToShow;
+        // Ad event fired when the splash ad has failed to load.
+        event EventHandler<YumiAdFailedToShowEventArgs> OnAdFailedToShow;
+        // Ad event fired when the splash ad is closed.
+        event EventHandler<EventArgs> OnAdClosed;
+        // Ad event fired when the splash ad is clicked.
+        event EventHandler<EventArgs> OnAdClicked;
+
+        // Creates an Splash.
+        void CreateSplashAd(string placementId, string channelId, string versionId, YumiSplashOptions splashOptions);
+        //load and show full screen splash
+        void LoadAdAndShow();
+        // Destroies the splash ad.
+        void DestroySplashAd();
+    }
+}
