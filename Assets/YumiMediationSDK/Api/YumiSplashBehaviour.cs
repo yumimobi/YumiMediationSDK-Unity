a48ec579e1db73207673c9c8d807d31e2896d187
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-19 15:54:01 +0800

    Add yumi splash scene

---
+++ Assets/YumiMediationSDK/Api/YumiSplashBehaviour.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using YumiMediationSDK.Api;
+using YumiMediationSDK.Common;
+using System;
+using UnityEngine.SceneManagement;
+
+public class YumiSplashBehaviour : MonoBehaviour
+{
+
+    private YumiSplashAd splashAd;
+    private string SplashPlacementId = "";
+    private string GameVersionId = "";
+    private string ChannelId = "";
+
+    void Start()
+    {
+        GameVersionId = YumiMediationSDKSetting.GetGameVersion;
+        ChannelId = YumiMediationSDKSetting.ChannelId();
+        SplashPlacementId = YumiMediationSDKSetting.SplashPlacementId();
+
+        LoadSplash();
+    }
+
+    public void Dispose()
+    {
+        if (splashAd != null)
+        {
+            splashAd.DestroySplashAd();
+            splashAd = null;
+        }
+    }
+
+    private void LoadSplash()
+    {
+        if (splashAd == null)
+        {
+
+            YumiSplashOptionsBuilder builder = new YumiSplashOptionsBuilder().setAdBottomViewHeight(100);
+            YumiSplashOptions splashOptions = new YumiSplashOptions(builder);
+
+            splashAd = new YumiSplashAd(SplashPlacementId, ChannelId, GameVersionId, splashOptions);
+            // add splash event
+            splashAd.OnAdSuccessToShow += HandleSplashAdSuccssToShow;
+            splashAd.OnAdFailedToShow += HandleSplashAdFailToShow;
+            splashAd.OnAdClicked += HandleSplashAdClicked;
+            splashAd.OnAdClosed += HandleSplashAdClosed;
+        }
+
+        splashAd.LoadAdAndShow();
+    }
+
+    private void InputMainSence()
+    {
+        SceneManager.LoadScene("YumiScene");
+    }
+    #region  splash callback handlers
+
+    public void HandleSplashAdSuccssToShow(object sender, EventArgs args)
+    {
+        Logger.Log("HandleSplashSuccssToShow event success to show");
+    }
+
+    public void HandleSplashAdFailToShow(object sender, YumiAdFailedToShowEventArgs args)
+    {
+       
+        Logger.Log("HandleSplashAdFailToShow + fail error is =  " + args.Message);
+        InputMainSence();
+    }
+
+    public void HandleSplashAdClicked(object sender, EventArgs args)
+    {
+        Logger.Log("HandleSplashAdClicked clicked");
+    }
+    public void HandleSplashAdClosed(object sender, EventArgs args)
+    {
+        Logger.Log("HandleSplashAdClosed Ad closed ");
+        InputMainSence();
+    }
+
+    #endregion
+}
