c85f35d18dd000c7f52cfeb1f5adc06a8fecf0ab
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-19 14:36:17 +0800

    Debugcenter  can test splash

--- Assets/YumiMediationSDK/Common/IYumiDebugCenterClient.cs
+++ Assets/YumiMediationSDK/Common/IYumiDebugCenterClient.cs
@@ -5,7 +5,7 @@ namespace YumiMediationSDK.Common
     public interface IYumiDebugCenterClient
     {
         // call debugcenter to test ads
-        void CallYumiMediationDebugCenter(string bannerPlacementID, string interstitialPlacementID, string videoPlacementID, string nativePlacementID, string channelID, string versionID);
+        void CallYumiMediationDebugCenter(string bannerPlacementID, string interstitialPlacementID, string videoPlacementID, string nativePlacementID, string splashPlacementID ,string channelID, string versionID);
 
         // Changes target server host
         void ChangeToTestServer();
