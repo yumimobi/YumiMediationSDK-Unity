6b28a1de8434fa976db46248b6ec27cd8dc9cacd
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-21 16:10:51 +0800

    Add interface of gdpr manager client

---
+++ Assets/YumiMediationSDK/Common/IYumiGDPRManagerClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YumiMediationSDK.Common
+{
+    public interface IYumiGDPRManagerClient
+    {
+        /// <summary>
+        /// Create gdpr manager instance
+        /// </summary>
+        void CreateGDPRManager();
+        /// <summary>
+        /// Update sdk gdpr consent
+        /// </summary>
+        void UpdateNetworksConsentStatus(YumiConsentStatus consentStatus);
+
+    }
+    public enum YumiConsentStatus
+    {
+        PERSONALIZED,
+        ///The user has granted consent for non-personalized ads.
+        NONPERSONALIZED,
+        /// The user has neither granted nor declined consent for personalized or non-personalized ads.
+        UNKNOWN
+    
+    }
+}
