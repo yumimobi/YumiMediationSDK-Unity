cb1ca6b6162abda604cb7872c919bb6785d2d23a
ShunZhi Tang <tangshunzhi@zplay.com>
2019-07-26 17:38:56 +0800

    Add event args files

---
+++ Assets/YumiMediationSDK/Api/YumiAdFailedToRenderEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace YumiMediationSDK.Api
+{
+    public class YumiAdFailedToRenderEventArgs : EventArgs
+    {
+        /// <summary>
+        /// yumi netive data
+        /// </summary>
+        public YumiNativeData nativeData { get; set; }
+        /// <summary>
+        /// error message
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
